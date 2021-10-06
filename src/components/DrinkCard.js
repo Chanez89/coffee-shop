52e47cb6e2c40045599281bf04fea67a7de426ae
Yuxiang Li <seanliyuxiang@gmail.com>
2021-10-06 17:54:14 -0500

    Fixed some DrinkCard component

--- src/components/DrinkCard.js
+++ src/components/DrinkCard.js
@@ -1,10 +1,10 @@
 import {useState} from 'react';
 // component responsible for rendering each drink card
 function DrinkCard(props) {
-  // const BASE_URL = 'http://localhost:4000/drinks'
+  const BASE_URL = 'http://localhost:4000/drinks'
   // destructuring `props`
   const {
-    drinkId,
+    drinkID,
     drinkTitle,
     drinkDescription,
     drinkIngredients,
@@ -15,14 +15,15 @@ function DrinkCard(props) {
     drinkLiked
   } = props;
   
- const [isLiked, setLiked] = useState(false);
+  const [isLiked, setLiked] = useState(false);
 
 
 
- function loveItHandler () {
-   setLiked(!isLiked)
+  function loveItHandler () {
+    setLiked(!isLiked)
+  }
 
-  //  fetch(`${BASE_URL}/${drinkId}`, {
+  //  fetch(`${BASE_URL}/${drinkID}`, {
   //    method: "PATCH",
   //    headers: {
   //      "Content-Type": "application/json"
@@ -34,46 +35,38 @@ function DrinkCard(props) {
   //  .then(response => response.json())
   //  .then(data => {debugger;})
 
- }
 
-//  function descriptionHandler() {
-//   console.log("description was clicked!") 
-//   return (
-//      console.log({drinkDescription})
-     
-//    )
-//   }
- 
-function drinkHandler() {
+  function descriptionHandler() {
+    console.log("description was clicked!")
+    return (
+      console.log({drinkDescription})
+    )
+  }
 
-  let drinkTemp = drinkHot ? "☕":"❄️";
-  return drinkTemp
+  function drinkHandler() {
+    let drinkTemp = drinkHot ? "☕":"❄️";
+    return drinkTemp
+  }
   
-}
-
-let ingredientsArrayJSX = drinkIngredients.map((ingredient, idx) => {
+  let ingredientsArrayJSX = drinkIngredients.map((ingredient, idx) => {
+    return (
+      <li key={idx} style={{listStyle: "none"}}>{ingredient}</li>
+    )
+  })
+  
   return (
-    <li key={idx} style={{listStyle: "none"}}>{ingredient}</li>
-  )
- })
- 
- return (
-   
-    <li classsName="card" 
-        style={{
-         margin: "1rem",
-         backgroundColor: "#EFC3A4"   
-        }}
+    <li classsName="card"
+      style={{
+      margin: "1rem",
+      backgroundColor: "#EFC3A4"   
+      }}
     >
       <img src={drinkImage} alt={drinkTitle}
         width="200px" height="200px"
         // style={{alignSelf: "center"}}
         // need to figure out how to center picture in card
-       />
+      />
       <h3 id="drinkTitle">{drinkTitle}</h3>
-
-
-
       <ul style= {{fontStyle: "italic"}}>{ingredientsArrayJSX}</ul>
       {/*  need to add an empty space between DrinkIngredients */}
       {/* <p>Description: {drinkDescription}</p> */}
@@ -82,7 +75,7 @@ let ingredientsArrayJSX = drinkIngredients.map((ingredient, idx) => {
       <p>Hot? {drinkHandler()}</p>
       {/* directs us to the ternary function drinkHandler */}
       <button onClick={descriptionHandler}>Description</button>
-        {/* ternary function that holds an empty heart or a full heart when liked/unliked */}
+      {/* ternary function that holds an empty heart or a full heart when liked/unliked */}
       {isLiked ? (<button onClick={loveItHandler}>❤</button>) : (<button onClick={loveItHandler}>♡</button>)}
     </li>
   );
