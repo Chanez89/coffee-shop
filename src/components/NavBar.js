8d887ede7d7fb5b79ac2849ac76a309744ca4190
Jacob <Jacobonasrallah@gmail.com>
2021-10-06 08:53:35 -0700

    Put in bootstrap and started navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,26 @@ import {NavLink} from 'react-router-dom';
 
 function NavBar() {
   return (
-    <div>
-      <NavLink to='/' exact activeStyle={{color: 'red'}}>
-        Home
+    <div class="container" >
+      <nav style={{backgroundColor: "#fefae0", }} >
+          <a class="navbar-brand" href="#">
+            <img src="./logo192.png" alt="" width="30" height="24"/>
+          </a>
+      <NavLink to='/' exact activeStyle={{color: 'brown'}}>
+        Home    
       </NavLink>
-      <NavLink to='/CoffeeShop' exact activeStyle={{ color: 'red' }}>
-        Coffee Shop
+      <NavLink to='/CoffeeShop' exact activeStyle={{ color: 'brown' }}>
+        Coffee Shop      
       </NavLink>
-      <NavLink to='/NewDrinkForm' exact activeStyle={{ color: 'red' }}>
-        New Drink Form
+      <NavLink to='/NewDrinkForm' exact activeStyle={{ color: 'brown' }}>
+        New Drink Form  
       </NavLink>
-      <NavLink to='/About' exact activeStyle={{ color: 'red' }}>
+      <NavLink to='/About' exact activeStyle={{ color: 'brown' }}>
         About
       </NavLink>
+      </nav>
     </div>
+      
   );
 }
 
