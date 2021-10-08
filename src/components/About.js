0932f6a9b4f99d2ea680d01fa46ee071ba865f74
Jacob <Jacobonasrallah@gmail.com>
2021-10-07 18:10:12 -0700

    end of thursday

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,30 @@
 function About() {
   return (
-    <h1>About component</h1>
+    <div >
+      <h1 className="lead">About this project</h1>
+        <p></p>
+
+      <div className="">
+        <div className="d-flex justify-content-between">
+          <h2 className="mt-4">Neil:</h2>
+          <p className="mt-4 p-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis nec turpis sed vulputate. Donec aliquet massa id faucibus malesuada. Suspendisse magna enim, sodales placerat massa ac, egestas luctus nulla. Aliquam eu elit nunc. Etiam massa sem, bibendum sit amet ornare feugiat, dictum nec mauris. Nunc pellentesque eros eu lacus gravida, sed facilisis sapien pellentesque. Suspendisse lobortis, ipsum sed tempus aliquet, magna dui elementum erat, a gravida purus augue quis urna. Morbi eu odio commodo, egestas augue a, interdum odio. </p>
+          <span><img  src="https://picsum.photos/200"/></span>
+        </div>
+        <div className="d-flex justify-content-between">
+          <img src="https://picsum.photos/200"/>
+          <h2 className="mt-4 p-3">Sean:</h2>
+          <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis nec turpis sed vulputate. Donec aliquet massa id faucibus malesuada. Suspendisse magna enim, sodales placerat massa ac, egestas luctus nulla. Aliquam eu elit nunc. Etiam massa sem, bibendum sit amet ornare feugiat, dictum nec mauris. Nunc pellentesque eros eu lacus gravida, sed facilisis sapien pellentesque. Suspendisse lobortis, ipsum sed tempus aliquet, magna dui elementum erat, a gravida purus augue quis urna. Morbi eu odio commodo, egestas augue a, interdum odio.</p>
+
+        </div>
+        <div className="d-flex justify-content-between">
+          <h2 className="mt-4 p-3">Jacob:</h2>
+          <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis nec turpis sed vulputate. Donec aliquet massa id faucibus malesuada. Suspendisse magna enim, sodales placerat massa ac, egestas luctus nulla. Aliquam eu elit nunc. Etiam massa sem, bibendum sit amet ornare feugiat, dictum nec mauris. Nunc pellentesque eros eu lacus gravida, sed facilisis sapien pellentesque. Suspendisse lobortis, ipsum sed tempus aliquet, magna dui elementum erat, a gravida purus augue quis urna. Morbi eu odio commodo, egestas augue a, interdum odio. </p>
+          <img src="https://picsum.photos/200"/>
+        </div>
+        </div>  
+      </div>
   );
 }
+
 
 export default About;
