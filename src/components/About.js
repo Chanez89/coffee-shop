936d9e8ce5399713fdb72ce3ec8ac697990ffd7b
Yuxiang Li <seanliyuxiang@gmail.com>
2021-10-09 21:41:02 -0500

    Spent some time polishing the app

--- src/components/About.js
+++ src/components/About.js
@@ -2,22 +2,22 @@ function About() {
   return (
     <div >
       <h1 className="lead">About this project</h1>
-      <p>This project was done in collaboration between Neil Chanez, Yuxiang Li and Jacob Nasrallah. We wanted to demonstrate the use of React, Javascript, and CSS styling with Bootstrap 5.</p>
+      <p>This project was done in collaboration between Neil Chanez, Yuxiang (Sean) Li and Jacob Nasrallah. We wanted to demonstrate the use of React, Javascript, and CSS styling with Bootstrap 5.</p>
       <div className="">
         <h1 className="lead">Neil Chanez</h1>
         <div className="justify-content-between">
-          <img src="./images/Neil.jpg" width="350px" height="350px" />
-          <p className="mt-4 p-3">Located in Los Angeles, California. You can find me at <ul className="list-inline"><li><a href="https://github.com/Chanez89">GitHub</a> </li><li><a href="https://www.linkedin.com/in/neil-chanez-gracia-04743b18/">LinkedIn</a></li></ul></p>
+          <img src="./images/Neil.jpg" width="350px" height="350px" alt="Neil" />
+          <p className="mt-4">Located in Los Angeles, California. You can find me at: <ul className="list-inline"><li><a href="https://github.com/Chanez89">GitHub</a> </li><li><a href="https://www.linkedin.com/in/neil-chanez-gracia-04743b18/">LinkedIn</a></li></ul></p>
         </div>
         <h1 className="lead">Yuxiang (Sean) Li</h1>
         <div className="justify-content-between">
-          <img src="./images/yuxiang(sean)_li.png" width="40%" height="40%" />
-          <p className="mt-4">Located in San Francisco, California. You can find me at <ul className="list-inline"><li><a href="https://github.com/seanliyuxiang">GitHub</a> </li><li><a href="https://www.linkedin.com/in/seanliyuxiang/">LinkedIn</a></li></ul></p>
+          <img src="./images/yuxiang(sean)_li.png" width="40%" height="40%" alt="Yuxiang-Sean" />
+          <p className="mt-4">Located in San Francisco, California. You can find me at: <ul className="list-inline"><li><a href="https://github.com/seanliyuxiang">GitHub</a> </li><li><a href="https://www.linkedin.com/in/seanliyuxiang/">LinkedIn</a></li></ul></p>
         </div>
         <h1 className="lead">Jacob Nasrallah</h1>
         <div className=" justify-content-between">
-          <img src="./images/jacobpfp.jpeg" />
-          <p className="mt-4">Located in Seattle, Washington. You can find me at <ul className="list-inline"><li><a href="https://github.com/nasjac26">GitHub</a> </li><li><a href="https://www.linkedin.com/in/jacob-nasrallah-6792b5106/">LinkedIn</a></li></ul></p>
+          <img src="./images/jacobpfp.jpeg" alt="Jacob" />
+          <p className="mt-4">Located in Seattle, Washington. You can find me at: <ul className="list-inline"><li><a href="https://github.com/nasjac26">GitHub</a> </li><li><a href="https://www.linkedin.com/in/jacob-nasrallah-6792b5106/">LinkedIn</a></li></ul></p>
         </div>
       </div>  
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 function Home() {
   return (
     <div className="font-styling text-styling">
-    <h1 className="text-center">"A place where I can get a cup of coffee as fresh as I am." - Sean</h1>
-    <p className="text-center">Order online to have our handcrafted drinks ready when you are.</p>
-    <img src="https://images.unsplash.com/photo-1513530176992-0cf39c4cbed4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2340&q=80" className="img-thumbnail"></img>
+      <h1 className="text-center">"A place where I can get a cup of coffee as fresh as I am." - Sean, Co-Founder</h1>
+      <p className="text-center">Order online to have our handcrafted drinks ready when you are.</p>
+      <img src="https://images.unsplash.com/photo-1513530176992-0cf39c4cbed4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2340&q=80" className="img-thumbnail" alt="scattered-coffee-beans" />
     </div>
   );
 }
